4b9de7eccc249b672965411ac846144186fd4ce6
guychienll <guychienll@gmail.com>
2023-12-28 17:39:56 +0800

    fix: fix the edge case when jsx source code with weird format

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,33 @@
+import template, { TemplateBuilderOptions } from '@babel/template';
+import * as t from '@babel/types';
+import * as prettier from 'prettier';
 import * as vscode from 'vscode';
 import { ProviderResult, TextEditor } from 'vscode';
-import * as prettier from 'prettier';
-import * as t from '@babel/types';
-import template from '@babel/template';
-
 import fs from 'fs';
+
+const SUPPORT_LANGUAGES = [
+    { scheme: 'file', language: 'typescript' },
+    { scheme: 'file', language: 'javascript' },
+    { scheme: 'file', language: 'typescriptreact' },
+    { scheme: 'file', language: 'javascriptreact' },
+];
 
 enum COMMANDS {
     DECLARE_UNDEFINED_COMPONENT = 'react-refactor-tool.declare-undefined-component',
 }
 
-export function getSelectedText() {
-    const editor = vscode.window.activeTextEditor;
-    if (editor) {
-        const selection = editor.selection;
-        return editor.document.getText(selection);
-    } else {
-        return null;
-    }
-}
+const PRETTIER_OPTIONS = {
+    parser: 'babel',
+    semi: true,
+    singleQuote: true,
+    jsxSingleQuote: true,
+    trailingComma: 'es5',
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    arrowParens: 'always',
+} as prettier.Options;
 
-export const parsingOptions = {
+export const BABEL_PARSING_OPTIONS = {
     plugins: [
         'objectRestSpread',
         'classProperties',
@@ -29,7 +36,7 @@ export const parsingOptions = {
         'optionalChaining',
     ],
     sourceType: 'module',
-} as any;
+} as TemplateBuilderOptions;
 
 function getLinesCountWithPath(file: string): Promise<number> {
     return new Promise((resolve) => {
@@ -85,47 +92,17 @@ export class CodeActionProvider implements vscode.CodeActionProvider {
         if (error && error.code === 2304) {
             const componentName = editor.document.getText(error.range);
 
-            const isSelfClosingJSXElement = editor.document
-                .getText()
-                .match(new RegExp(`<(${componentName})([^>]*?)(\/{1})(\s*)?>`));
+            const range = this.getRangeOfJSXElement(document, componentName);
 
-            let rangeOfJSXElement: vscode.Range;
-
-            if (isSelfClosingJSXElement) {
-                isSelfClosingJSXElement[0].length;
-                rangeOfJSXElement = new vscode.Range(
-                    editor.document.positionAt(
-                        isSelfClosingJSXElement.index || 0
-                    ),
-                    editor.document.positionAt(
-                        (isSelfClosingJSXElement.index || 0) +
-                            isSelfClosingJSXElement[0].length
-                    )
-                );
-            } else {
-                rangeOfJSXElement = new vscode.Range(
-                    editor.document.positionAt(
-                        editor.document
-                            .getText()
-                            .match(new RegExp(`<${componentName}>`))?.index || 0
-                    ),
-                    editor.document.positionAt(
-                        (editor.document
-                            .getText()
-                            .match(new RegExp(`<\/${componentName}>`))?.index ||
-                            0) +
-                            componentName.length +
-                            3
-                    )
-                );
+            if (!range) {
+                return;
             }
 
-            const targetJSXElementPlainText =
-                editor.document.getText(rangeOfJSXElement);
+            const targetJSXElementPlainText = editor.document.getText(range);
 
             const ast: any = template.ast(
                 targetJSXElementPlainText,
-                parsingOptions
+                BABEL_PARSING_OPTIONS
             );
 
             const title = 'Declare component with function declaration';
@@ -145,6 +122,49 @@ export class CodeActionProvider implements vscode.CodeActionProvider {
 
         return [];
     }
+
+    private getRangeOfJSXElement(
+        document: vscode.TextDocument,
+        componentName: string
+    ) {
+        let rangeOfJSXElement: vscode.Range | null = null;
+
+        const selfClosingJSXElementMatchObject = document
+            .getText()
+            .match(new RegExp(`<(${componentName})([^>]*?)(\/{1})(\\s*)?>`));
+
+        if (selfClosingJSXElementMatchObject) {
+            selfClosingJSXElementMatchObject[0].length;
+            rangeOfJSXElement = new vscode.Range(
+                document.positionAt(
+                    selfClosingJSXElementMatchObject.index || 0
+                ),
+                document.positionAt(
+                    (selfClosingJSXElementMatchObject.index || 0) +
+                        selfClosingJSXElementMatchObject[0].length
+                )
+            );
+        } else {
+            const startMatchObject = document
+                .getText()
+                .match(new RegExp(`<(\\s*)${componentName}(\\s*)>`));
+            const endMatchObject = document
+                .getText()
+                .match(new RegExp(`<\/(\\s*)${componentName}(\\s*)>`));
+
+            if (!startMatchObject || !endMatchObject) {
+                return rangeOfJSXElement;
+            }
+
+            rangeOfJSXElement = new vscode.Range(
+                document.positionAt(startMatchObject.index || 0),
+                document.positionAt(
+                    (endMatchObject.index || 0) + endMatchObject[0].length
+                )
+            );
+        }
+        return rangeOfJSXElement;
+    }
 }
 
 export function activate(context: vscode.ExtensionContext) {
@@ -158,16 +178,7 @@ export function activate(context: vscode.ExtensionContext) {
                 }
                 const appendText = await prettier.format(
                     `function ${symbol} (){ return(<></>) }`,
-                    {
-                        parser: 'babel',
-                        semi: true,
-                        singleQuote: true,
-                        jsxSingleQuote: true,
-                        trailingComma: 'es5',
-                        bracketSpacing: true,
-                        jsxBracketSameLine: false,
-                        arrowParens: 'always',
-                    }
+                    PRETTIER_OPTIONS
                 );
 
                 appendTextToCurrentActiveFile(editor, appendText);
@@ -177,12 +188,7 @@ export function activate(context: vscode.ExtensionContext) {
 
     context.subscriptions.push(
         vscode.languages.registerCodeActionsProvider(
-            [
-                { scheme: 'file', language: 'typescript' },
-                { scheme: 'file', language: 'javascript' },
-                { scheme: 'file', language: 'typescriptreact' },
-                { scheme: 'file', language: 'javascriptreact' },
-            ],
+            SUPPORT_LANGUAGES,
             new CodeActionProvider()
         )
     );
